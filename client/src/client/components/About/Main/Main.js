cddfe0ef6b5d130ef6bfc31e198f9d1b51f59fcd
jmsct <jmsctdev@gmail.com>
2020-03-31 20:32:07 -0400

    updated main

--- client/src/client/components/About/Main/Main.js
+++ client/src/client/components/About/Main/Main.js
@@ -36,7 +36,7 @@ export default function AboutPage() {
                     I enjoy doing ctf's in my spare time and writing scripts using either bash or python to simplify workflow that becomes too repetitive.
                     <br />
                     <br />
-                    I am not an expert but my willingness to learn and adapt may surely be an asset to you, your team and  your company; with time, relevant skills, right mindset and a chance, I believe anyone can be valuable to any company.
+                    My willingness to learn and adapt may surely be an asset to you, your team and  your company; with time, relevant skills, right mindset and a chance, I believe anyone can be valuable to any company.
                     <br />
                     <br />
                     I am currently open to opportunities within the cyber security discipline and if you feel as though my listed skills or willingness to learn/improve would be a great match for your company then it would be my utmost pleasure to hear from you.
