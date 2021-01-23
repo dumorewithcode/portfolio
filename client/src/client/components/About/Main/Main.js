30c9140dbe6eb4f77ef185ccc1da0d60b1f61053
a3kSec <jmsctdev@gmail.com>
2021-01-22 19:02:52 -0500

    Updated About

--- client/src/client/components/About/Main/Main.js
+++ client/src/client/components/About/Main/Main.js
@@ -33,17 +33,13 @@ export default function AboutPage() {
                     Adaptable and self motivated learner that's passionate about the latest and greatest technologies with an emphasis on security research/analysis, penetration testing and automation.
                     <br />
                     <br />
-                    I enjoy performing security audits on web/mobile apps and writing scripts using either bash or python to simplify workflow that becomes too repetitive.
+                    I enjoy doing ctf challenges in my spare time, performing security audits on web/mobile apps and writing scripts using either bash, powershell or python to simplify workflow that becomes too repetitive.
                     <br />
                     <br />
-                    I am currently scripting an auditor that works as a wrapper around many open source tools and my own scripts to automate the process of finding common security flaws within web apps. This is a private tool that is a work in progress 
-                    with a planned open source release when it's ready.
+                    My willingness to learn and adapt may surely be an asset to you, your team and your company. With time, relevant skills, right mindset and a chance, I believe anyone can be valuable to any company.
                     <br />
                     <br />
-                    My willingness to learn and adapt may surely be an asset to you, your team and  your company; with time, relevant skills, right mindset and a chance, I believe anyone can be valuable to any company.
-                    <br />
-                    <br />
-                    I am currently open to opportunities preferably within the cyber security discipline and if you feel as though my listed skills and willingness to learn/improve would be a great match for your company then it would be my utmost pleasure to hear from you.
+                    I am currently open to opportunities within the cyber security discipline and if you feel as though my listed skills would be a great match for your company, then it would be my utmost pleasure to hear from you.
                 </Typography>
             </div>
         </div>
